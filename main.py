8b2eb3d1a2a294ea20eb6fe2df85b99151d6a847
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-03-16 02:49:20 +0530

    commit 5

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def close_issue(num):
     issue.create_comment('bonk !! You cannont create more than 3 Issues at a time close your old issues to make a new one')
     issue.edit(state = 'closed')
 
+
 github = github.Github(token)
 repo = github.get_repo(os.environ['GITHUB_REPOSITORY'])
+
 count_issues(name=author,token=token)
+
 numb = get_latest_issue(name=author,token=token)
 if count_issues >= 4:
     close_issue(numb)
