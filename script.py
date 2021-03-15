4566a44f29c64a1c686beb131bf7f4315ea22c7c
anushkrishnav <anush.venkatakrishna@gmail.com>
2021-03-16 02:30:24 +0530

    commit 5

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 
 def count_issues(name, token):
     owner = name
-    repo = "Issue-spam-blocker"
+    repo = "supdem"
     author = name
     query_url = f"https://api.github.com/search/issues?q=is:issue+repo:{owner}/{repo}+author:{author}+is:open"
 
